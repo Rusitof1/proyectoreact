15abdfb5a1b300ee45f644250435b61b94b8c843
Matias <matiasreidman@gmail.com>
2022-10-18 01:41:52 -0300

    pagina totalmente nueva pocos errores

--- src/components/Paginas.js
+++ src/components/Paginas.js
@@ -2,6 +2,7 @@ import React from "react";
 import { Route, Routes } from "react-router-dom";
 import Inicio from "./Paginas/Inicio";
 import { ProductosLista } from "./Paginas/Productos";
+import { ProductoItem } from "./Paginas/Productos/ProductoItem";
 import { ProductosDetalles } from "./Paginas/Productos/ProductosDetalles";
 
 export function Paginas() {
