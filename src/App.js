b980c355b28b2bef2c244914aa579540c1c2c26a
Matias <matiasreidman@gmail.com>
2022-10-26 19:59:35 -0300

    andando routing y nav sin miniatura de imagenes

--- src/App.js
+++ src/App.js
@@ -2,22 +2,27 @@ import React from "react";
 import { Header } from "./components/Header/Index";
 import { Carrito } from "./components/Carrito/Index";
 import { DataProvider } from "./context/DataProvider";
-import { BrowserRouter as Router } from "react-router-dom";
-import { Paginas } from "./components/Paginas";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Inicio from "./components/paginas/Inicio/Index";
+import { ProductosLista } from "./components/paginas/Productos/Index";
+import { ProductosDetalles } from "./components/paginas/Productos/ProductosDetalles";
 import "boxicons";
 
 function App() {
   return (
-    <DataProvider>
-      <div className="App">
-        <Router>
+    <>
+      <BrowserRouter>
+        <DataProvider>
           <Header />
           <Carrito />
-          <Paginas />
-        </Router>
-      </div>
-    </DataProvider>
+          <Routes>
+            <Route exact path="/" element={<Inicio />} />
+            <Route exact path="/productos" element={<ProductosLista />} />
+            <Route exact path="/producto:id" element={<ProductosDetalles />} />
+          </Routes>
+        </DataProvider>
+      </BrowserRouter>
+    </>
   );
 }
-
 export default App;
--- src/components/paginas/Inicio/index.js
+++ src/components/paginas/Inicio/index.js
@@ -1,17 +1,11 @@
 import React from "react";
-import { Link } from "react-router-dom";
 import Portada from "../../../images/inicio.jpg";
 
 export default function Inicio() {
   return (
-    <div className="Inicio">
-      <Link to="/">
-        <h1>Home</h1>
-      </Link>
-      <Link to="/productos">
-        <h1>Productos</h1>
-      </Link>
-      <img src={Portada} alt="portada" />
+    <div className="inicio">
+      <h1>|||| ||| || | | | SNEAKERS & SHOES | | | || ||| ||||</h1>
+      <img src={Portada} alt="Portada" />
     </div>
   );
 }
